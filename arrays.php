b3540bc912a3795b19fc9ee89f090a32b950b760
Daniel Gawne <danielgawne@gmail.com>
2022-03-06 17:31:26 -0500

    finish arrays

--- arrays.php
+++ arrays.php
@@ -7,6 +7,28 @@
 */
 
   // Indexed
+  //
   $people = array('Alpha', 'Beta', 'Omega');
-  echo $people[1];
+  $cars = ['honda', 'toyota', 'ford'];
+  $cars[3] = 'chevy';
+  $cars[] = 'fiat';
+  // echo count($cars);
+  // print_r($cars); prints entire array
+  // var_dump($cars);
+
+  // Associative
+  //
+  $places = ['first' => 1, 'third' => 3, 'fifth' => 5];
+  $places['second'] = 2;
+  // echo $places['third'];
+
+  // Multi-Dimensional
+  //
+  $years = [
+    array(2022, 'jan', 'feb', 'march'),
+    array(2021, 'jan', 'feb', 'march', 'april', 'may',),
+    array(2020, 'jan', 'feb', 'march', 'april', 'may',)
+  ];
+  // echo $years[0][1];
+  // print_r($years);
 ?>
